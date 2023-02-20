03d0719fcde7aecfba3c1b5b67652c32bd5a6fd0
michal-nwok <michal.nwk@gmail.com>
2023-02-20 17:03:16 +0100

    last changes

--- src/pages/Summary/Summary.tsx
+++ src/pages/Summary/Summary.tsx
@@ -6,7 +6,7 @@ import { ScoreInformation } from "../../models/ScoreInformation";
 function Summary() {
   const [scoreData, setScoreData] = useState<ScoreInformation[]>(data);
   return (
-    <div className="font-grotesk xl:flex xl:flex-row xl:shadow-xl xl:rounded-3xl">
+    <div className="font-grotesk xl:flex xl:flex-row xl:shadow-xl xl:rounded-3xl xl:w-4/5">
       <section className="bg-gradient-to-t from-royal-blue to-slate-blue flex flex-col place-items-center px-16 py-5 rounded-b-3xl xl:rounded-3xl xl:w-2/5">
         <h1 className="font-bold text-lg text-light-lavender">Your Result</h1>
         <div className="bg-gradient-to-t from-persian-blue to-violet-blue rounded-full flex flex-col p-8 place-items-center my-4 w-36">
@@ -18,14 +18,20 @@ function Summary() {
           You scored higher than 65% of the people who have taken these tests.
         </p>
       </section>
-      <section className="p-5 w-full xl:w-3/5">
+      <section className="p-5 w-full xl:w-3/5 flex flex-col justify-evenly">
         <h2 className="text-xl mb-6 font-bold">Summary</h2>
-        {scoreData.map((scoreInfo: ScoreInformation) => {
-          return (
-            <ScoreCard scoreInformation={scoreInfo} key={scoreInfo.category} />
-          );
-        })}
-        <button className="w-full bg-dark-gray p-4 rounded-5xl text-white hover:bg-gradient-to-t hover:from-royal-blue hover:to-slate-blue mb-6">
+        <div>
+          {scoreData.map((scoreInfo: ScoreInformation) => {
+            return (
+              <ScoreCard
+                scoreInformation={scoreInfo}
+                key={scoreInfo.category}
+              />
+            );
+          })}
+        </div>
+
+        <button className="w-full bg-dark-gray p-4 rounded-5xl text-white hover:bg-gradient-to-t hover:from-royal-blue hover:to-slate-blue">
           Continue
         </button>
       </section>
